ccb1128213016c87dc35d5f6c92ec6500651544d
Tushar Kant <nanu.clickity@gmail.com>
2018-02-15 06:07:23 +0530

    updates

--- src/server/app.js
+++ src/server/app.js
@@ -8,38 +8,36 @@ import { Config, initConfig } from './config'
 
 const debug = require('debug')('baza:server')
 
-const app  = express();
+const app = express()
 
+var serverInstance
 
-var serverInstance;
+const runServer = app => {
+  const port =
+    process.env.PORT && Number.isInteger(Number(process.env.PORT))
+      ? process.env.PORT
+      : Config.NODE_PORT
 
-const runServer = (app)=> {
-	const port = process.env.PORT && Number.isInteger(Number(process.env.PORT))
-		? process.env.PORT
-		: Config.NODE_PORT
-
-	debug(`
+  console.log(`
 		PORT from ENV   : ${process.env.PORT}
 		PORT from Config: ${Config.NODE_PORT}
 		Final Port      : ${port}
 	`)
-	
-	serverInstance = app.listen(port, function onListentingCB(){
-		debug(`Node server on http://127.0.0.1:${port}`)
-		if (__DEV__)
-			debug(`Hot  server on http://127.0.0.1:${Config.PROXY_PORT}`)
-	})
+
+  serverInstance = app.listen(port, function onListentingCB() {
+    debug(`Node server on http://127.0.0.1:${port}`)
+    if (__DEV__) debug(`Hot  server on http://127.0.0.1:${Config.PROXY_PORT}`)
+  })
 }
 
-
 // Startup steps
-initConfig(app)						// reads config.json
-	.then(readDependencies)			// reads webpack assets and other files
-	.then(configureServer)			// configures express server
-	.then(runServer)				// run server
-	.catch(console.error.bind(console))	// report any startup errors
+initConfig(app) // reads config.json
+  .then(readDependencies) // reads webpack assets and other files
+  .then(configureServer) // configures express server
+  .then(runServer) // run server
+  .catch(console.error.bind(console)) // report any startup errors
 
-export function getServerInstance(){
-	return serverInstance
+export function getServerInstance() {
+  return serverInstance
 }
 export default app
