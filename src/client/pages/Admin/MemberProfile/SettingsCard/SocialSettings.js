fd8218f89dbc4030e75f92df5a75daa7a4c3b732
Tushar Kant <nanu.clickity@gmail.com>
2018-03-06 16:35:19 +0530

    <SettingsCard/>

---
+++ src/client/pages/Admin/MemberProfile/SettingsCard/SocialSettings.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { CardContent } from 'components/ui/CardWithTabs'
+
+export default class SocialSettings extends Component {
+  render() {
+    return (
+      <CardContent>
+        <h2> SocialSettings Here </h2>
+      </CardContent>
+    )
+  }
+}
