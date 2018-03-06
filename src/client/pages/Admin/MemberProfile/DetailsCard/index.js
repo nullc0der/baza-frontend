dd0d601d332d781e3d547c4d4e2a3684e525a4fa
Tushar Kant <nanu.clickity@gmail.com>
2018-03-06 14:59:09 +0530

    MemberProfile details card

---
+++ src/client/pages/Admin/MemberProfile/DetailsCard/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardContent
+} from 'components/ui/CardWithTabs'
+
+const CONTACT_DETAILS = [
+  { label: 'Phone Office', icon: 'fa-building', value: '+91-98765432210' },
+  { label: 'Phone Home', icon: 'fa-building', value: '+91-98765432210' },
+  { label: 'Phone Mobile', icon: 'fa-building', value: '+91-98765432210' },
+  { label: 'Phone Emergency', icon: 'fa-building', value: '+91-98765432210' }
+]
+
+const ACTIVITIES_DETAILS = [
+  { label: 'Activation Date', icon: 'fa-building', value: '22 Mar 2016' },
+  { label: 'References', icon: 'fa-building', value: '1' }
+]
+
+export default class DetailsCard extends Component {
+  renderOneDetailItem = (detail, index) => {
+    return (
+      <div className="detail-item" key={index}>
+        <div className="label">
+          <i className={`fa ${detail.icon}`} />
+          <span className="ml-1">{detail.label}</span>
+        </div>
+        <div className="value">{detail.value}</div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <Card className="details-card">
+        <CardHeader title="DETAILS" subtitle="Following users verified me" />
+        <CardBody>
+          <CardContent>
+            <div className="details-section">
+              <div className="title">CONTACT</div>
+              <div className="details-list ml-1">
+                {CONTACT_DETAILS.map(this.renderOneDetailItem)}
+              </div>
+            </div>
+            <div className="details-section mt-2">
+              <div className="title">ACTIVITIES</div>
+              <div className="details-list ml-1">
+                {ACTIVITIES_DETAILS.map(this.renderOneDetailItem)}
+              </div>
+            </div>
+          </CardContent>
+        </CardBody>
+      </Card>
+    )
+  }
+}
--- src/client/pages/Admin/MemberProfile/index.js
+++ src/client/pages/Admin/MemberProfile/index.js
@@ -5,6 +5,7 @@ import s from './MemberProfile.scss'
 
 import ProfileCard from './ProfileCard'
 import DocumentsCard from './DocumentsCard'
+import DetailsCard from './DetailsCard'
 
 export default class MemberProfile extends Component {
   render() {
@@ -18,6 +19,12 @@ export default class MemberProfile extends Component {
           <div className="col-md-8">
             <DocumentsCard />
           </div>
+        </div>
+        <div className="row mt-2 align-stretch">
+          <div className="col-md-4">
+            <DetailsCard />
+          </div>
+          <div className="col-md-8" />
         </div>
         <h3>Things</h3>
       </div>
