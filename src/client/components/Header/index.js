9f496d03ba1b78f1db79570845e875183543110f
Tushar Kant <nanu.clickity@gmail.com>
2018-03-10 08:38:14 +0530

    Added more responsive breakpoints to homepage header. TG-11 #in-progress

--- src/client/components/Header/index.js
+++ src/client/components/Header/index.js
@@ -1,172 +1,177 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
-import classnames from "classnames";
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom'
 
-import debounce from "lodash/debounce";
-import { scrollToElement } from "utils/dom";
+import debounce from 'lodash/debounce'
+import { scrollToElement } from 'utils/dom'
 
-import "./Header.scss";
+import './Header.scss'
 
 const HEADER_ITEMS_LEFT = [
-    { title: "Home", href: "/#hero-section" },
-    { title: "Features", href: "/#features-section" },
-    { title: "Status", href: "/#status-section" }
-];
+  { title: 'Home', href: '/#hero-section' },
+  { title: 'Features', href: '/#features-section' },
+  { title: 'Status', href: '/#status-section' }
+]
 
 const HEADER_ITEMS_RIGHT = [
-    { title: "Latest", href: "/#latest-section" },
-    { title: "Login", href: "#!login" },
-    { title: "Contact", href: "/#contact-section" }
-];
+  { title: 'Latest', href: '/#latest-section' },
+  { title: 'Login', href: '#!login' },
+  { title: 'Contact', href: '/#contact-section' }
+]
 
 export default class Header extends Component {
-    static propTypes = {
-        scrollspy: PropTypes.bool,
-        invert: PropTypes.bool,
-        inCenter: PropTypes.bool,
-        showDonateButton: PropTypes.bool,
-        className: PropTypes.string
-    };
+  static propTypes = {
+    scrollspy: PropTypes.bool,
+    invert: PropTypes.bool,
+    inCenter: PropTypes.bool,
+    showDonateButton: PropTypes.bool,
+    className: PropTypes.string
+  }
 
-    state = {
-        isFixed: false
-    };
+  state = {
+    isFixed: false
+  }
 
-    componentDidMount = () => {
-        if (this.props.scrollspy) this.startListeningToScroll();
-    };
+  componentDidMount = () => {
+    if (this.props.scrollspy) this.startListeningToScroll()
+  }
 
-    componentWillUnmount = () => {
-        if (this._handleScroll) this.stopListeningToScroll();
-    };
+  componentWillUnmount = () => {
+    if (this._handleScroll) this.stopListeningToScroll()
+  }
 
-    startListeningToScroll = () => {
-        if (!this._handleScroll)
-            this._handleScroll = debounce(this.handleScroll, 60);
+  startListeningToScroll = () => {
+    if (!this._handleScroll)
+      this._handleScroll = debounce(this.handleScroll, 60)
 
-        window.addEventListener("scroll", this._handleScroll, false);
-    };
+    window.addEventListener('scroll', this._handleScroll, false)
+  }
 
-    stopListeningToScroll = () => {
-        window.removeEventListener("scroll", this._handleScroll, false);
-        this._handleScroll = null; // Garbage collection
-    };
+  stopListeningToScroll = () => {
+    window.removeEventListener('scroll', this._handleScroll, false)
+    this._handleScroll = null // Garbage collection
+  }
 
-    handleScroll = () => {
-        let top = $(window).scrollTop();
-        let threshold = $(".app-header").height();
+  handleScroll = () => {
+    let top = $(window).scrollTop()
+    let threshold = $('.app-header').height()
 
-        // console.log(top, threshold, top >= threshold);
+    // console.log(top, threshold, top >= threshold);
 
-        this.setState({ isFixed: top >= threshold });
-    }
+    this.setState({ isFixed: top >= threshold })
+  }
 
-    renderOneLink = (item, index) => {
-        return (
-            <Link className="header-link" to={item.href} key={index}>
-                {item.title}
+  renderOneLink = (item, index) => {
+    return (
+      <Link className="header-link" to={item.href} key={index}>
+        {item.title}
+      </Link>
+    )
+  }
+
+  renderOneBSLink = (item, index) => {
+    const cx = classnames('nav-item', {
+      active: item.active
+    })
+    return (
+      <li className={cx} key={index}>
+        <NavLink
+          className="nav-link"
+          to={item.href}
+          onClick={this.handleLinkNavigation}>
+          {item.title}
+        </NavLink>
+      </li>
+    )
+  }
+
+  handleLinkNavigation = e => {
+    const strip = window.location.origin + '/'
+    const href = (e.target.href || '').replace(strip, '')
+
+    const isValid = /^#[a-zA-Z]/.test(href)
+
+    $('.app-header .collapse.show').removeClass('show')
+
+    if (!isValid) return
+
+    const el = document.getElementById(href.substr(1))
+    if (el)
+      scrollToElement(el, {
+        offset: $('.app-header').height() * -1
+      })
+
+    // If element is not present, wait for browser to navigate
+    // and then scroll again
+    setTimeout(() => {
+      scrollToElement(href, {
+        offset: $('.app-header').height() * -1
+      })
+    }, 500)
+  }
+
+  render() {
+    const {
+      className,
+      inCenter,
+      invert = false,
+      showDonateButton = false
+    } = this.props
+
+    const cx = classnames('app-header navbar navbar-expand-md', className, {
+      'in-center': inCenter,
+      'navbar-light bg-white': invert || this.state.isFixed,
+      'fixed-top': this.state.isFixed
+    })
+
+    return (
+      <Fragment>
+        <nav className={cx} id="app-header">
+          <div className="container">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarContentContainer"
+              aria-controls="navbarContentContainer"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <div className="menu-icon" />
+            </button>
+
+            <div
+              className="collapse navbar-collapse"
+              id="navbarContentContainer">
+              <ul className="navbar-nav align-items-center">
+                {HEADER_ITEMS_LEFT.map(this.renderOneBSLink)}
+                <li className="nav-item center-icon">
+                  <NavLink
+                    className="nav-link"
+                    to="/"
+                    activeClassName="active"
+                    onClick={this.handleLinkNavigation}>
+                    <img
+                      className="img-fluid"
+                      alt="Baza"
+                      src="/public/img/baza_logo.svg"
+                    />
+                  </NavLink>
+                </li>
+                {HEADER_ITEMS_RIGHT.map(this.renderOneBSLink)}
+              </ul>
+            </div>
+          </div>
+          {showDonateButton && (
+            <Link
+              to="#!donate"
+              className="btn btn-outline-primary btn-rounded donate-button">
+              Donate Now
             </Link>
-        );
-    };
-
-    renderOneBSLink = (item, index) => {
-        const cx = classnames("nav-item", {
-            active: item.active
-        });
-        return (
-            <li className={cx} key={index}>
-                <NavLink className="nav-link" to={item.href} onClick={this.handleLinkNavigation}>
-                    {item.title}
-                </NavLink>
-            </li>
-        );
-    };
-
-    handleLinkNavigation = (e)=> {
-        const strip = window.location.origin + '/'
-        const href = (e.target.href || '').replace(strip, '')
-
-        const isValid = /^#[a-zA-Z]/.test(href)
-
-        $('.app-header .collapse.show').removeClass('show')
-        
-        if (!isValid) 
-            return
-        
-        const el = document.getElementById(href.substr(1))
-        if (el) 
-            scrollToElement(el, {
-                offset: $('.app-header').height()*-1 
-            })
-
-        // If element is not present, wait for browser to navigate
-        // and then scroll again
-        setTimeout(()=> {
-            scrollToElement(href, {
-                offset: $('.app-header').height()*-1 
-            })
-        }, 500)
-        
-    }
-
-    render() {
-        const {
-            className,
-            inCenter,
-            invert = false,
-            showDonateButton = false
-        } = this.props;
-
-        const cx = classnames("app-header navbar navbar-expand-md", className, {
-            "in-center": inCenter,
-            "navbar-light bg-white": invert || this.state.isFixed,
-            "fixed-top": this.state.isFixed
-        });
-
-        return (
-            <Fragment>
-                <nav className={cx} id="app-header">
-                    <div className="container">
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarContentContainer"
-                            aria-controls="navbarContentContainer"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation">
-                            <div className='menu-icon'/>
-                        </button>
-
-                        <div
-                            className="collapse navbar-collapse"
-                            id="navbarContentContainer">
-                            <ul className="navbar-nav mx-auto align-items-center">
-                                {HEADER_ITEMS_LEFT.map(this.renderOneBSLink)}
-                                <li className="nav-item center-icon d-none d-md-block d-lg-block d-xl-block">
-                                    <NavLink className="nav-link" to="/" activeClassName="active" onClick={this.handleLinkNavigation}>
-                                        <img
-                                            className="img-fluid"
-                                            alt="Baza"
-                                            src="/public/img/baza_logo.svg"
-                                        />
-                                    </NavLink>
-                                </li>
-                                {HEADER_ITEMS_RIGHT.map(this.renderOneBSLink)}
-                            </ul>
-                        </div>
-                    </div>
-                    {showDonateButton && (
-                        <Link
-                            to="#!donate"
-                            className="btn btn-outline-primary btn-rounded donate-button">
-                            Donate Now
-                        </Link>
-                    )}
-                </nav>
-            </Fragment>
-        );
-    }
+          )}
+        </nav>
+      </Fragment>
+    )
+  }
 }
