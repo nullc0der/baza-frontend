992f3e776c9e24d29479e94a91a9665b74ba609c
Tushar Kant <nanu.clickity@gmail.com>
2018-03-10 20:29:40 +0530

    updates

--- src/client/components/ui/ParallaxContainer/index.js
+++ src/client/components/ui/ParallaxContainer/index.js
@@ -1,64 +1,59 @@
-import React, {Component} from 'react'
-import PropTypes  from 'prop-types'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
 import './ParallaxContainer.scss'
 
 export default class ParallaxContainer extends Component {
-	static propTypes = {
-		backgroundImage: PropTypes.string.isRequired,
-		className: PropTypes.string
-	}
+  static propTypes = {
+    backgroundImage: PropTypes.string.isRequired,
+    className: PropTypes.string
+  }
 
-	componentDidMount = ()=> {
-		this.lastPosition = 0
-		this.isTicking = false
-		this.speedFactor = 2
+  componentDidMount = () => {
+    this.lastPosition = 0
+    this.isTicking = false
+    this.speedFactor = 2
 
-		window.addEventListener('scroll', this.handleScroll, false)
-	}
+    window.addEventListener('scroll', this.handleScroll, false)
+  }
 
-	componentWillUnmount = ()=> {
-		window.removeEventListener('scroll', this.handleScroll, false)
-	}
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll, false)
+  }
 
-	handleScroll = ()=> {
-		this.lastPosition = window.pageYOffset
+  handleScroll = () => {
+    this.lastPosition = window.pageYOffset
 
-		if (!this.isTicking){
-			window.requestAnimationFrame(this.updatePosition)
-			this.isTicking = true
-		}
-	}
+    if (!this.isTicking) {
+      window.requestAnimationFrame(this.updatePosition)
+      this.isTicking = true
+    }
+  }
 
-	updatePosition = ()=> {
-		var shift  = this.lastPosition / this.speedFactor
+  updatePosition = () => {
+    var shift = this.lastPosition / this.speedFactor
 
-		if (shift < 0)
-			shift = 0
+    if (shift < 0) shift = 0
 
-		this.backgroundElement.style.transform = `translate3d(0px, ${shift}px, 0px)`
-		this.isTicking = false
-	}
+    this.backgroundElement.style.transform = `translate3d(0px, ${shift}px, 0px)`
+    this.isTicking = false
+  }
 
-	render(){
-		const {
-			backgroundImage,
-			className,
-			children,
-			...others
-		} = this.props
+  render() {
+    const { backgroundImage, className, children, ...others } = this.props
 
-		const cx = classnames('ui-parallax-container', className)
+    const cx = classnames('ui-parallax-container', className)
 
-		return (
-			<div className={cx} {...others}>
-				<div className='ui-parallax-background'
-					ref={node => this.backgroundElement = node}
-					style={{ backgroundImage: `url('${backgroundImage}')` }}>
-				</div>
-				{children}
-			</div>
-		)
-	}
+    return (
+      <div className={cx} {...others}>
+        <div
+          className="ui-parallax-background"
+          ref={node => (this.backgroundElement = node)}
+          style={{ backgroundImage: `url('${backgroundImage}')` }}
+        />
+        {children}
+      </div>
+    )
+  }
 }
