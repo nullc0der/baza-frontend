12645b01ba3195a4ce0a11a08e07a7f0f78c9d3d
Prasanta <prasantakakati1994@gmail.com>
2018-10-16 02:42:45 +0530

    Landing video in full size

--- src/client/pages/HomePage/CurrentStatusSection.js
+++ src/client/pages/HomePage/CurrentStatusSection.js
@@ -14,7 +14,7 @@ const CurrentStatusSection = props => {
             <div className="container page-section">
                 <h3 className="text-center mb-3"> Current Status </h3>
                 <div className="row mb-5">
-                    <div className="col-12 col-md-8 mx-auto">
+                    <div className="col-12 mx-auto">
                         <div className="embed-responsive embed-responsive-16by9">
                             <video
                                 title="Baza Intro"
