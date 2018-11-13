d213168aa9b129d98387d63b21a61f02641218b5
Tushar <nanu.clickity@gmail.com>
2018-11-13 14:42:04 +0530

    pure svg trustmeter

--- src/client/pages/Admin/MemberProfile/TrustMeter/index.js
+++ src/client/pages/Admin/MemberProfile/TrustMeter/index.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 import BasicCard from '../BasicCard'
 
-import SVGDial from './SVGDial'
-
 import s from './TrustMeter.scss'
+import SVGDial from './SVGDial';
 
 class TrustMeter extends Component {
+    state = {
+        value: 40
+    }
     render() {
+        const { value } = this.state
         return (
             <BasicCard title='Trust Meter' className={s.container}>
-                <SVGDial />
+                <SVGDial value={value} />
+                <div className='trust-percentage'>{value}%</div>
+                <div className='trust-message'>you can be in top 10%</div>
             </BasicCard>
         )
     }
