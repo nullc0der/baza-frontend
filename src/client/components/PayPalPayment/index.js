1a57ef1b4b387ec5a269d2223a3c7753d92cf991
Prasanta <prasantakakati1994@gmail.com>
2018-11-30 11:00:59 +0530

    [ADDED] Paypal live client id

--- src/client/components/PayPalPayment/index.js
+++ src/client/components/PayPalPayment/index.js
@@ -24,7 +24,9 @@ class PayPalPayment extends Component {
     render() {
         const client = {
             sandbox:
-                'AbY19geS0t8PRkT2DAzf9Ztut-avodvX6IZfI7vpz_RGtYQZmuzZrZ9l8l5kAy0Bjcct3Gj8fROD4Jo1'
+                'AbY19geS0t8PRkT2DAzf9Ztut-avodvX6IZfI7vpz_RGtYQZmuzZrZ9l8l5kAy0Bjcct3Gj8fROD4Jo1',
+            production:
+                'AfiyuriLBkZZMxcYEywYepywpuoZP8w0q3xzFdt8PJyAxz-w3017K6i78ZCzHQwecFJY8azGy9BZ6oF3'
         }
         const PayPalButton = window.paypal.Button.driver('react', {
             React,
@@ -37,7 +39,7 @@ class PayPalPayment extends Component {
                     payment={this.payment}
                     commit={true}
                     onAuthorize={this.onAuthorize}
-                    env={'sandbox'}
+                    env={'production'}
                     style={{
                         size: 'responsive',
                         color: 'black',
