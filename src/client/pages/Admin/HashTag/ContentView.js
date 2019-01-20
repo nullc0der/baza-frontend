cf4bb5e9135b27acca433761b0f30e5a13fce938
Tushar <nanu.clickity@gmail.com>
2019-01-21 00:42:15 +0530

    updates

--- src/client/pages/Admin/HashTag/ContentView.js
+++ src/client/pages/Admin/HashTag/ContentView.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+
+import ColorPicker from 'components/ui/ColorPicker'
+
 import CircularImage from 'components/ui/CircularImage'
 import ProfileGallery from './ProfileGallery'
 
 import s from './HashTag.scss'
 import EditImage from './EditImage';
+import SVGTemplate from './SVGTemplate';
+import { Promise } from 'q';
 
 // function imageToDataURL(imageSrc) {
 //     const img = document.createElement('img')
@@ -52,6 +57,48 @@ import EditImage from './EditImage';
 //     return url
 // }
 
+function getFinalImagePNG() {
+    const svg = document.getElementById('final-image-svg')
+    const svgStr = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+
+    canvas.width = 128
+    canvas.height = 128
+
+    const _URL = (window.URL || window.webkitURL)
+    const img = new Image()
+
+    var svgBlob = new Blob([svgStr], { type: 'image/svg+xml;charset=utf-8' })
+    var url = _URL.createObjectURL(svgBlob)
+
+    return new Promise((resolve, reject) => {
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0)
+            let finalImage = canvas.toDataURL("image/png")
+            _URL.revokeObjectURL(url)
+            resolve(finalImage)
+        }
+
+        img.src = url
+    })
+}
+
+function downloadAs(filename, data) {
+    const el = document.createElement('a')
+    el.setAttribute('href', data)
+    el.setAttribute('download', filename)
+
+    el.style.display = 'none'
+    document.body.appendChild(el)
+
+    el.click();
+
+    setTimeout(() => {
+        document.body.removeChild(el)
+    }, 500)
+}
+
 class HashTagContent extends Component {
     static propTypes = {
         selectedProvider: PropTypes.string,
@@ -60,7 +107,11 @@ class HashTagContent extends Component {
     state = {
         previewImage: '',
         croppedImage: '',
-        showCropper: false
+        showCropper: false,
+        textColor: '#888',
+        semiCircleColor: '#fff',
+        showBGColorPicker: false,
+        showTextColorPicker: false
     }
 
     onImageUpload = (e) => {
@@ -99,13 +150,19 @@ class HashTagContent extends Component {
         this.setState({ showCropper: false, previewImage: '' })
     }
 
-    getFinalImageURL = () => {
+    downloadImage = () => {
         const { croppedImage } = this.state
         if (!croppedImage) {
-            return '#'
+            return;
         }
 
-        return croppedImage
+        getFinalImagePNG().then(data => {
+            downloadAs('baza-avatar.png', data)
+        }).catch(err => {
+            alert(err.message)
+        })
+
+        // return croppedImage
     }
 
     getImageFromSocial = () => {
@@ -116,10 +173,34 @@ class HashTagContent extends Component {
         console.log('will upload image to social network')
     }
 
+    onSemiCircleColorChange = (color) => {
+        console.log('semi circle', color)
+        const c = color.rgb;
+        const semiCircleColor = `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})`
+        this.setState({ semiCircleColor })
+    }
+
+    onTextColorChange = (color) => {
+        // console.log('text color', color)
+        const c = color.rgb;
+        const textColor = `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})`
+        this.setState({ textColor })
+    }
+
     render() {
         const cx = classnames(s.contentView)
-        const { previewImage, showCropper, croppedImage } = this.state
+        const {
+            previewImage,
+            showCropper,
+            croppedImage,
+            textColor,
+            semiCircleColor,
+            showTextColorPicker,
+            showBGColorPicker
+        } = this.state
+
         const { selectedProvider } = this.props
+
         return (
             <div className={cx}>
                 <h3 className='content-title'>
@@ -134,7 +215,12 @@ class HashTagContent extends Component {
                     <div className='arrow'>
                         <i className='fa fa-arrow-right'></i>
                     </div>
-                    <CircularImage size={128} src={croppedImage} />
+                    <SVGTemplate
+                        id='final-image-svg'
+                        textColor={textColor}
+                        semiCircleColor={semiCircleColor}
+                        imageData={croppedImage} />
+                    {/* <CircularImage size={128} src={croppedImage} /> */}
                 </div>
                 <div className='actions mt-3'>
                     <div className='btn btn-light btn-upload-local'>
@@ -146,25 +232,40 @@ class HashTagContent extends Component {
                         Use from {selectedProvider.name}
                         <i className='fa fa-facebook-f'></i>
                     </div>
-                    <div className='btn btn-light' onClick={this.setBannerBG}>
+                    <div
+                        className='btn btn-light position-relative'
+                        onClick={() => this.setState({ showBGColorPicker: true })}>
                         Change Background Color
                         <i className='fa fa-eyedropper'></i>
+                        {
+                            showBGColorPicker && <ColorPicker
+                                color={semiCircleColor}
+                                onChange={this.onSemiCircleColorChange}
+                                onRequestClose={() => this.setState({ showBGColorPicker: false })} />
+                        }
                     </div>
-                    <div className='btn btn-light' onClick={this.setBannerTextColor}>
+                    <div
+                        className='btn btn-light position-relative'
+                        onClick={() => this.setState({ showTextColorPicker: true })}>
                         Change Text Color
                         <i className='fa fa-eyedropper'></i>
+                        {
+                            showTextColorPicker && <ColorPicker
+                                color={textColor}
+                                onChange={this.onTextColorChange}
+                                onRequestClose={() => this.setState({ showTextColorPicker: false })} />
+                        }
                     </div>
                 </div>
                 <ProfileGallery
                     onImageSelect={this.onGalleryImageSelect}
                 />
                 <div className='final-actions actions'>
-                    <a
+                    <div
                         className='btn btn-dark btn-large btn-download'
-                        download='baza-avatar.png'
-                        href={this.getFinalImageURL(croppedImage)}>
+                        onClick={this.downloadImage}>
                         Download Image
-                    </a>
+                    </div>
                     <div
                         className={`btn btn-dark btn-large ${selectedProvider.className}`}
                         onClick={this.uploadImageToSocial}>Upload to {selectedProvider.name}</div>
---
+++ src/client/pages/Admin/HashTag/SVGTemplate.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+const SVGTemplate = ({ id, semiCircleColor, textColor, imageData }) => (
+    <svg id={id} className='svg-template-image' width="129px" height="128px" viewBox="0 0 129 128" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <defs></defs>
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="ImageTemplate" transform="translate(-3.000000, 0.000000)">
+                <g id="imageGroup" transform="translate(4.000000, 0.000000)">
+                    <image id="Bitmap" x="0" y="0" width="128" height="128" xlinkHref={imageData} />
+                </g>
+                <g id="outerContainer" transform="translate(3.000000, 0.000000)" fill="transparent" fill-rule="nonzero">
+                    <path d="M64.86,128 L0.86,128 L0.86,64 C0.86,99.346224 29.513776,128 64.86,128 Z" id="Shape"></path>
+                    <path d="M0.86,64 L0.86,0 L64.86,0 C29.513776,-2.164332e-15 0.86,28.653776 0.86,64 Z" id="Shape"></path>
+                    <path d="M128.86,0 L128.86,64 C128.86,28.653776 100.206224,2.164332e-15 64.86,0 L128.86,0 Z" id="Shape"></path>
+                    <path d="M128.86,64 L128.86,128 L64.86,128 C100.206224,128 128.86,99.346224 128.86,64 Z" id="Shape"></path>
+                </g>
+                <path d="M108.86,64 C108.86,86.6436747 90.5036747,105 67.86,105 C45.2163253,105 26.86,86.6436747 26.86,64 L3.86,64 C3.86,99.346224 32.513776,128 67.86,128 C103.206224,128 131.86,99.346224 131.86,64 L108.86,64 Z" id="semiCircle" fill={semiCircleColor} fill-rule="nonzero" opacity="0.26"></path>
+                <g id="text" transform="translate(0.000000, 63.000000)" fill={textColor} font-family="Raleway-Bold, Raleway" font-size="22" font-weight="bold">
+                    <g id="#" transform="translate(15.599455, 12.442278) rotate(76.660751) translate(-15.599455, -12.442278) translate(7.099455, -1.057722)">
+                        <text>
+                            <tspan x="0.11262279" y="21.534036">#</tspan>
+                        </text>
+                    </g>
+                    <g id="B" transform="translate(22.182548, 27.236382) rotate(59.331073) translate(-22.182548, -27.236382) translate(14.182548, 13.736382)">
+                        <text>
+                            <tspan x="0.3238832" y="21.5284354">B</tspan>
+                        </text>
+                    </g>
+                    <g id="a" transform="translate(31.262372, 38.444931) rotate(43.550000) translate(-31.262372, -38.444931) translate(24.262372, 24.944931)">
+                        <text>
+                            <tspan x="0.2001137" y="21.8008698">a</tspan>
+                        </text>
+                    </g>
+                    <g id="s" transform="translate(41.101479, 46.014348) rotate(30.250000) translate(-41.101479, -46.014348) translate(35.101479, 32.514348)">
+                        <text>
+                            <tspan x="0.9491338" y="21.444897">s</tspan>
+                        </text>
+                    </g>
+                    <g id="i" transform="translate(49.485060, 49.486917) rotate(20.730000) translate(-49.485060, -49.486917) translate(45.985060, 35.986917)">
+                        <text>
+                            <tspan x="0.0282938" y="21.4526169">i</tspan>
+                        </text>
+                    </g>
+                    <g id="c" transform="translate(58.912430, 52.550140) rotate(10.310000) translate(-58.912430, -52.550140) translate(51.912430, 39.050140)">
+                        <text>
+                            <tspan x="0.1783262" y="21.0397303">c</tspan>
+                        </text>
+                    </g>
+                    <g id="I" transform="translate(67.992228, 52.506321) rotate(-0.930000) translate(-67.992228, -52.506321) translate(63.992228, 39.006321)">
+                        <text>
+                            <tspan x="0.8855393" y="21.9819862">I</tspan>
+                        </text>
+                    </g>
+                    <g id="n" transform="translate(78.454882, 51.575315) rotate(-11.534621) translate(-78.454882, -51.575315) translate(70.954882, 38.075315)">
+                        <text>
+                            <tspan x="0.8488889" y="21.7610148">n</tspan>
+                        </text>
+                    </g>
+                    <g id="c" transform="translate(91.145506, 47.574017) rotate(-26.140000) translate(-91.145506, -47.574017) translate(84.145506, 34.074017)">
+                        <text>
+                            <tspan x="0.7321654" y="21.2236269">c</tspan>
+                        </text>
+                    </g>
+                    <g id="o" transform="translate(102.434518, 40.244140) rotate(-40.539152) translate(-102.434518, -40.244140) translate(94.934518, 26.744140)">
+                        <text>
+                            <tspan x="0.1205037" y="21.4607251">o</tspan>
+                        </text>
+                    </g>
+                    <g id="m" transform="translate(113.993154, 26.951652) rotate(-59.450000) translate(-113.993154, -26.951652) translate(102.993154, 13.451652)">
+                        <text>
+                            <tspan x="0.892091" y="21.7318206">m</tspan>
+                        </text>
+                    </g>
+                    <g id="e" transform="translate(119.125501, 10.173933) rotate(-78.465379) translate(-119.125501, -10.173933) translate(111.625501, -3.326067)">
+                        <text>
+                            <tspan x="0.111684" y="21.5662903">e</tspan>
+                        </text>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+)
+
+export default SVGTemplate
