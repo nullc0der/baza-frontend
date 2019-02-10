940389e0abbd1c64ce0c933a5285f23a829796d1
Prasanta <prasantakakati1994@gmail.com>
2019-02-10 15:00:18 +0530

    New fix for page blank issue

--- src/client/components/Avatar/index.js
+++ src/client/components/Avatar/index.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'
 import classnames from 'classnames'
 import { connect } from 'react-redux'
-import get from 'lodash/get'
 
 import Config from 'utils/config'
 
 import s from './Avatar.scss'
 
+import { getUsername } from 'utils/common'
+
 class Avatar extends Component {
     render() {
         const { className, size = 24, own = true, otherProfile } = this.props
         const userProfile = own ? this.props.userProfile : otherProfile
-        const userName =
-            get(userProfile, 'username', '') ||
-            get(userProfile.user, 'username', '')
+        const userName = getUsername(userProfile)
         const cx = classnames(s.container, 'ui-avatar', className, {
             'img-fluid': userProfile.profile_photo
         })
@@ -33,20 +32,20 @@ class Avatar extends Component {
                 }}
             />
         ) : (
-                <div
-                    className={cx}
-                    style={{
-                        width: size + 'px',
-                        height: size + 'px',
-                        fontSize: size / 12 + 'px',
-                        backgroundColor: userProfile.default_avatar_color
-                    }}>
-                    <div className="avatar-name">
-                        {' '}
-                        {userName && userName[0].toUpperCase()}{' '}
-                    </div>
+            <div
+                className={cx}
+                style={{
+                    width: size + 'px',
+                    height: size + 'px',
+                    fontSize: size / 12 + 'px',
+                    backgroundColor: userProfile.default_avatar_color
+                }}>
+                <div className="avatar-name">
+                    {' '}
+                    {userName && userName[0].toUpperCase()}{' '}
                 </div>
-            )
+            </div>
+        )
     }
 }
 
