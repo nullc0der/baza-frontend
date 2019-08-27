f54e1fba0c0cacc0f22728d70b806cdc47430216
prasanta <prasantakakati1994@gmail.com>
2019-08-28 01:14:15 +0530

    [ADDED] Basic staff bar and login system

--- src/client/pages/Admin/DistributionSignUp/index.js
+++ src/client/pages/Admin/DistributionSignUp/index.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react'
 import classnames from 'classnames'
 import Helmet from 'react-helmet'
+import isEmpty from 'lodash/isEmpty'
+
+import {
+    getStaffBarData,
+    loginOutStaff
+} from 'api/distribution-signup-staff-side'
 
 import DistributionSignUpList from './DistributionSignupList'
 import DistributionSignUp from './DistributionSignUp'
+import StaffBar from './StaffBar'
 import s from './DistributionSignUp.scss'
 
 class DistributionSignupsPage extends Component {
+    state = {
+        isLoggedIn: false,
+        staffBarData: {}
+    }
+
+    componentDidMount = () => {
+        getStaffBarData()
+            .then(response => {
+                this.setState({
+                    staffBarData: response.data
+                })
+            })
+            .catch(() => {})
+    }
+
+    toggleLoginLogout = request_type => {
+        loginOutStaff(request_type).then(() => {
+            this.setState({
+                isLoggedIn: request_type === 'login' ? true : false
+            })
+        })
+    }
+
     render() {
         const cx = classnames(s.container)
+        const { isLoggedIn, staffBarData } = this.state
         return (
             <div className={cx}>
                 <Helmet title="Distribution Signups" />
-                <div className="row h-100">
-                    <div className="col-md-3">
-                        <DistributionSignUpList />
+                {!isEmpty(staffBarData) && (
+                    <div className="row">
+                        <div className="col-md-12">
+                            <StaffBar
+                                isLoggedIn={isLoggedIn}
+                                staffBarData={staffBarData}
+                                toggleLoginLogout={this.toggleLoginLogout}
+                            />
+                        </div>
                     </div>
-                    <div className="col-md-9">
-                        <DistributionSignUp />
+                )}
+                {!!isLoggedIn && (
+                    <div className="row h-100">
+                        <div className="col-md-3">
+                            <DistributionSignUpList />
+                        </div>
+                        <div className="col-md-9">
+                            <DistributionSignUp />
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         )
     }
