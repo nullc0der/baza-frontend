d45087e349a6eeaca3d3d1000908559fbcdf83a7
prasanta <prasantakakati1994@gmail.com>
2019-12-26 21:46:01 +0530

    [TAIGA-579] Remove Captcha widget on member side

--- src/client/components/AdminFooter/index.js
+++ src/client/components/AdminFooter/index.js
@@ -28,27 +28,42 @@ export default class Footer extends Component {
         const cx = classnames(
             s.container,
             className,
-            'ui-footer flex-horizontal',
-            'a-stretch'
+            'ui-footer',
+            'a-stretch',
+            'flex-horizontal'
         )
         const release = Config.get('RELEASE')
         const releaseCodename = Config.get('RELEASE_CODENAME')
         const siteType = Config.get('SITE_TYPE')
         return (
             <div className={cx}>
-                <div className="flex-horizontal a-center">
+                <div className="flex-horizontal a-center flex-1">
                     <b>Copyright &copy; 2014-2018 Baza.&nbsp;</b>
                     <span>All rights reserved.</span>
                     <span className="badge badge-pill pill-blue pill-powered">
                         Powered by Ekata
                     </span>
+                    <span>
+                        This site is protected by reCAPTCHA and the Google{' '}
+                        <a
+                            href="https://policies.google.com/privacy"
+                            target="_blank"
+                            className="link">
+                            Privacy Policy
+                        </a>{' '}
+                        and{' '}
+                        <a
+                            href="https://policies.google.com/terms"
+                            target="_blank"
+                            className="link">
+                            Terms of Service
+                        </a>{' '}
+                        apply.
+                    </span>
                 </div>
-                <div className="flex-1" />
                 <div className="flex-horizontal a-center">
                     <div
-                        className={`badge badge-pill pill-${
-                            SITE_TYPE_TEXT_AND_COLOR[siteType].color
-                        } pill-site-type`}>
+                        className={`badge badge-pill pill-${SITE_TYPE_TEXT_AND_COLOR[siteType].color} pill-site-type`}>
                         {SITE_TYPE_TEXT_AND_COLOR[siteType].text}
                     </div>
                     <div className="badge badge-pill pill-blue">
